6db31a7d1ca0661628b246cbb7442b3480379d04
Raihan454 <raihanmarsa712@gmail.com>
2022-10-17 20:52:25 +0700

    move the pivot

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -88,8 +88,22 @@ namespace QuickSort
                     mov_count++;
                 }
             }
+            // j now contains the index of the last elements int the sorted list
 
-        }
+            if (low < j)
+            {
+                //Move the pivot to its correct position in the list
+                swap(low, j);
+                mov_count++;
+            }
+            //sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
+        
+
+    }
         }
 
     }
