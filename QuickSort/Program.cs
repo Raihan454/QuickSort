d384c223232246380eceb0c7bfd68d1bfa9433eb
Raihan454 <raihanmarsa712@gmail.com>
2022-10-17 20:44:10 +0700

    add class program

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,3 +1,15 @@
 ﻿using System;
 
-namespace QuickSort { }
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        //Number of elements in array
+        private int n;
+    }
+}
