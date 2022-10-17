750db89d76ff0a914bc6f9b1d981003cb550c32f
Raihan454 <raihanmarsa712@gmail.com>
2022-10-17 20:45:42 +0700

    add void read

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -11,5 +11,21 @@ namespace QuickSort
 
         //Number of elements in array
         private int n;
-    }
+
+        void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\n Array can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n====================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n====================");
+        }
 }
